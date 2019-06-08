f804ec7e98faf4f07ce38573d09aa0b623d37e48
asuka001 <a.asuka54@gmail.com>
2019-06-08 09:52:50 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      flash[:success] = "ユーザを登録しました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ユーザの登録に失敗しました"
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  private
+  
+  def params_user
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+end
